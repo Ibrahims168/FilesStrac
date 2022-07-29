25e444adbafff18c9f14147b5ebe0353a4c44ac3
Ibrahims168 <96667811+Ibrahims168@users.noreply.github.com>
2022-07-29 20:51:20 +0300

    new project

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './brand.css'
+
+const Brand = () => {
+  (
+    <div>
+        Brand
+    </div>
+  )
+}
+import './Brand.css'
+
+export default Brand
---
+++ src/containers/whatIAS8/WhatIAS8.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'whatIAS8.css'
+
+
+const WhatIAS8 = () => {
+  return (
+    <div>WhatIAS8</div>
+  )
+}
+
+export default WhatIAS8
